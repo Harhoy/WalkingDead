a914ac3c2bc832f79734183f3dc997ab3c266d0c
Harhoy <haraldhoyem@gmail.com>
2018-06-30 21:17:21 +0200

    Update Zombie.java

--- Zombie.java
+++ Zombie.java
@@ -1,19 +1,23 @@
 import java.lang.Math;
 
+//ZOMBIE-CLASS
+
 public class Zombie {
 
-  protected double x;
-  protected double y;
+  protected double x; //X-coordinate
+  protected double y; //Y-coordinate
 
   public Zombie(double initX, double initY) {
     x = initX;
     y = initY;
   }
 
+  //Find distance to another zombies/point
   public double getDistance(double xB, double yB) {
     return Math.pow(Math.pow((x-xB),2)+Math.pow((y-yB),2),0.5);
   }
 
+   //Return position
   public void hentPos() {
 
     System.out.println(x + " " + y);
